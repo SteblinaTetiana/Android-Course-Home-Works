46367e8f73aa6228d9c6c34262e8d70d6f03c70c
Steblyna Tetiana <tanyshechka16@gmail.com>
2021-11-22 19:11:29 +0200

    add ViewModel, Repository

--- p091directorysviewmodel/src/main/java/com/triare/p091directorysviewmodel/MainActivity.kt
+++ p091directorysviewmodel/src/main/java/com/triare/p091directorysviewmodel/MainActivity.kt
@@ -2,10 +2,52 @@ package com.triare.p091directorysviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
+import android.widget.*
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private var viewModel:MainViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image = findViewById<ImageView>(R.id.image)
+        val title = findViewById<TextView>(R.id.heading)
+        val description = findViewById<TextView>(R.id.description)
+
+       viewModel= ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel?.animalsModel?.observe(this){
+            image.setImageResource(it.image!!)
+            title.text = it.title
+            description.text = it.description
+        }
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+
+        val buttonAnimals=findViewById<RadioButton>(R.id.animals)
+        val buttonPlants=findViewById<RadioButton>(R.id.plants)
+
+        menu?.setGroupVisible(R.id.group_animals, buttonAnimals.isChecked)
+        menu?.setGroupVisible(R.id.group_plants, buttonPlants.isChecked)
+
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        viewModel?.addAnimals(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
 }
---
+++ p091directorysviewmodel/src/main/java/com/triare/p091directorysviewmodel/MainViewModel.kt
@@ -0,0 +1,17 @@
+package com.triare.p091directorysviewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+     private val repository = Repository()
+
+    val animalsModel = MutableLiveData<Animals>()
+
+    fun addAnimals(id:Int?){
+        val animal = repository.getAnimalById(id)
+        animalsModel.postValue(animal)
+
+    }
+}
